d5bf402368c95a07e878be8ab5e8000acc7a8ffc
Boris K <boris.koyfman@gmail.com>
2016-12-18 00:12:57 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+var main = function() {
+	$('.more-btn').click(function(){
+    $('.more-menu').toggle().next();
+  });
+  
+  $('.share').click(function() {
+    $('.share-menu').toggle();
+  });
+  
+  $('.notification').click(function(){
+    $(this).toggleClass('active');
+  });
+  
+  
+  
+};
+
+$(document).ready(main);
